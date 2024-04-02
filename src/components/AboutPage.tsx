9620cb46c9678954c99f94a8d22c5d88325b56b0
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-04-02 10:38:14 +0300

    feat: write markup with jsx

---
+++ src/components/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+function AboutPage() {
+    return (
+        <div>
+            <h1>This is the About Page</h1>
+            <p>Hey people. <br/> I hope y'all are doing well.</p>
+            <img className ="avatar" />
+        </div>
+    );
+}
+
+export default AboutPage;
