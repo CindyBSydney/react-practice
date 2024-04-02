3e92fcbeda14caebb6e261eaf5c4c56042df0b3c
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-04-02 10:43:55 +0300

    feat: conditional rendering

---
+++ src/components/Login.tsx
@@ -0,0 +1,38 @@
+import AboutPage from "./AboutPage";
+
+//using if else statement
+
+/*
+let content;
+
+if (isLoggedIn) {
+    content = <AboutPage />;
+
+} else {
+    content = <LoginPage />;
+}
+
+
+return (
+    <div>
+        {content}
+    </div>
+);
+
+//alternatively
+
+<div>
+    {isLoggedIn ? (
+        <AboutPage />
+    ) : (
+        <LoginPage />
+    )}  
+</div>
+
+
+//if the else branch is not needed
+<div>
+    {isLoggedIn && <AboutPage />}
+</div>  
+
+*/
